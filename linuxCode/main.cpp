949dc3745a511d69b6607de02ddbceb4ca83ddf9
unknown <contato.nevespedro@gmail.com>
2022-05-12 09:11:56 -0300

    primeiro commit

---
+++ linuxCode/main.cpp
@@ -0,0 +1,176 @@
+/*Q3. Faça um programa para escrever a contagem regressiva do lançamento de um foguete. O programa deve imprimir 10, 9, 8, ..., 1, 0 e Fogo! na tela.
+DICA: Pesquise sobre ASCII art para deixar seu programa mais interessante.
+*/
+#include <iostream>
+#include <unistd.h>  // biblioteca pro usleep e sleep      cin.get() enter para contagem.
+using namespace std;
+  int main(){
+    int i =10 ;
+    
+    cout << endl;
+    cout << "*****************"<<endl<<endl;
+    cout << "iniciar contagem para o lançamento!"<<endl<<endl<<endl;
+  sleep (1);
+    while (i >= 0){ 
+     cout<< i << endl;
+     sleep (1);
+
+     i--;
+   }
+   cout << "iniciando lançamento !!! " << endl;
+   cout << endl<<endl<<endl;
+   sleep (1);
+   // lembrar de usar sleep em cada linha
+
+       cout <<"                                 !" << endl;
+       sleep (1);
+       cout <<"                                 !" << endl;
+sleep (1);
+       cout <<"                                 ^" << endl;
+       sleep (1);
+       cout << "                               /   \\"<<endl;
+       sleep (1);
+       cout << "                              /_____\\"<< endl;
+       sleep (1);
+       cout << "                              |=   =|" << endl;
+       sleep (1);
+       cout << "                              |  A  |" << endl;
+       sleep (1);
+       cout << "                              |  C  |" << endl;
+       sleep (1);
+       cout << "                              |  M  |" << endl;
+       sleep (1);
+       cout << "                              |  E  |" << endl;
+       sleep (1);
+       cout << "                              |     |" << endl;
+       sleep (1);
+       cout << "                              |     |" << endl;
+       sleep (1);
+       cout << "                              |     |" << endl;
+       sleep (1);
+       cout << "                              |     |" << endl;
+       sleep (1);
+       cout << "                              |     |" << endl;
+       sleep (1);
+       cout << "                             /|##!##|\\" << endl;
+       sleep (1);
+       cout << "                             /|##!##|\\" << endl;
+       sleep (1);
+       cout <<"                            / |##!##| \\" << endl;
+       sleep (1);
+       cout <<"                           | / ^ | ^ \\ |" << endl;
+       sleep (1);
+       cout << "                           |/  ( | )  \\|" << endl;
+       sleep (1);
+       cout << "                           |/   ( | ) \\|" << endl;
+       sleep (1);
+       cout << "                               ((   ))" << endl;
+       sleep (1);
+       cout << "                             ((  :  ))" << endl;
+       sleep (1);
+       cout << "                           ((  :  ))" << endl;
+       sleep (1);
+       cout << "                             ((   ))" << endl;
+       sleep (1);
+       cout << "                               (( ))" << endl;
+       sleep (1);
+       cout << "                            ( )" << endl;
+       cout << "                               ((   ))" << endl;
+       sleep (1);
+       cout << "                             ((  :  ))" << endl;
+       sleep (1);
+       cout << "                           ((  :  ))" << endl;
+       sleep (1);
+       cout << "                             ((   ))" << endl;
+       sleep (1);
+       cout << "                              (( ))" << endl;
+       sleep (1);
+       cout << "                              ( )" << endl;
+       sleep (1);
+       cout << "                            ((   ))" << endl;
+       sleep (1);
+       cout << "                           ((  :  ))" << endl;
+       sleep (1);
+       cout << "                            ((  :  ))" << endl;
+       sleep (1);
+       cout << "                             ((   ))" << endl;
+       sleep (1);
+       cout << "                               (( ))" << endl;
+       sleep (1);
+       cout << "                               ( )" << endl;
+       sleep (1);
+       cout << "                                ." << endl;
+       sleep (1);
+       cout << "                                ." << endl;
+       sleep (1);
+       cout << "                                ." << endl;
+
+      cout << endl;
+             sleep (1);
+
+      cout << endl;
+             sleep (1);
+
+       cout << endl;
+              sleep (1);
+
+       cout << endl;
+              sleep (1);
+       cout << endl;
+        sleep (1);
+       cout << endl;
+        sleep (1);
+       cout << endl;
+        sleep (1);
+       cout << endl;
+        sleep (1);
+       cout << endl;
+        sleep (1);
+       cout << endl;
+        sleep (1);
+       cout << endl;
+        sleep (1);
+       cout << endl;
+        sleep (1);
+       cout << endl;
+        sleep (1);
+       cout << endl;
+        sleep (1);     
+       cout << endl;
+        sleep (1);
+       cout << endl;
+        sleep (1);
+       cout << endl;
+        sleep (1);
+       cout << endl;
+        sleep (1);
+       cout << endl;
+        sleep (1);
+       cout << endl;
+        sleep (1);
+       cout << endl;
+        sleep (1); 
+      cout << endl;
+        sleep (1);
+       cout << endl;
+        sleep (1);
+       cout << endl;
+        sleep (1);
+   
+       cout << "                      lançamento bem sucedido!"<< endl;
+       cout << endl;
+       cout << endl;
+       cout << endl;
+       cout << endl;
+       cout << endl;
+       cout << endl;
+       cout << endl;
+       cout << endl;
+       cout << endl;
+       cout << endl;
+       cout << endl;
+       cout << endl;
+       cout << endl;
+       cout << endl;
+       cout << endl;      
+  }
---
+++ rocket.cpp
@@ -0,0 +1,177 @@
+/*Q3. Faça um programa para escrever a contagem regressiva do lançamento de um foguete. O programa deve imprimir 10, 9, 8, ..., 1, 0 e Fogo! na tela.
+DICA: Pesquise sobre ASCII art para deixar seu programa mais interessante.
+*/
+#include <iostream>
+#include <unistd.h>  // biblioteca pro usleep e Sleep   000   cin.get() enter para contagem.
+#include <windows.h>
+using namespace std;
+  int main(){
+    int i =10 ;
+    
+    cout << endl;
+    cout << "*****************"<<endl<<endl;
+    cout << "iniciar contagem para o lançamento!"<<endl<<endl<<endl;
+  Sleep (1000);
+    while (i >= 0){ 
+     cout<< i << endl;
+     Sleep (1000);
+
+     i--;
+   }
+   cout << "iniciando lançamento !!! " << endl;
+   cout << endl<<endl<<endl;
+   Sleep (1000);
+   // lembrar de usar Sleep em000 cada linha
+
+       cout <<"                                 !" << endl;
+       Sleep (1000);
+       cout <<"                                 !" << endl;
+Sleep (1000);
+       cout <<"                                 ^" << endl;
+       Sleep (1000);
+       cout << "                               /   \\"<<endl;
+       Sleep (1000);
+       cout << "                              /_____\\"<< endl;
+       Sleep (1000);
+       cout << "                              |=   =|" << endl;
+       Sleep (1000);
+       cout << "                              |  A  |" << endl;
+       Sleep (1000);
+       cout << "                              |  C  |" << endl;
+       Sleep (1000);
+       cout << "                              |  M  |" << endl;
+       Sleep (1000);
+       cout << "                              |  E  |" << endl;
+       Sleep (1000);
+       cout << "                              |     |" << endl;
+       Sleep (1000);
+       cout << "                              |     |" << endl;
+       Sleep (1000);
+       cout << "                              |     |" << endl;
+       Sleep (1000);
+       cout << "                              |     |" << endl;
+       Sleep (1000);
+       cout << "                              |     |" << endl;
+       Sleep (1000);
+       cout << "                             /|##!##|\\" << endl;
+       Sleep (1000);
+       cout << "                             /|##!##|\\" << endl;
+       Sleep (1000);
+       cout <<"                            / |##!##| \\" << endl;
+       Sleep (1000);
+       cout <<"                           | / ^ | ^ \\ |" << endl;
+       Sleep (1000);
+       cout << "                           |/  ( | )  \\|" << endl;
+       Sleep (1000);
+       cout << "                           |/   ( | ) \\|" << endl;
+       Sleep (1000);
+       cout << "                               ((   ))" << endl;
+       Sleep (1000);
+       cout << "                             ((  :  ))" << endl;
+       Sleep (1000);
+       cout << "                           ((  :  ))" << endl;
+       Sleep (1000);
+       cout << "                             ((   ))" << endl;
+       Sleep (1000);
+       cout << "                               (( ))" << endl;
+       Sleep (1000);
+       cout << "                            ( )" << endl;
+       cout << "                               ((   ))" << endl;
+       Sleep (1000);
+       cout << "                             ((  :  ))" << endl;
+       Sleep (1000);
+       cout << "                           ((  :  ))" << endl;
+       Sleep (1000);
+       cout << "                             ((   ))" << endl;
+       Sleep (1000);
+       cout << "                              (( ))" << endl;
+       Sleep (1000);
+       cout << "                              ( )" << endl;
+       Sleep (1000);
+       cout << "                            ((   ))" << endl;
+       Sleep (1000);
+       cout << "                           ((  :  ))" << endl;
+       Sleep (1000);
+       cout << "                            ((  :  ))" << endl;
+       Sleep (1000);
+       cout << "                             ((   ))" << endl;
+       Sleep (1000);
+       cout << "                               (( ))" << endl;
+       Sleep (1000);
+       cout << "                               ( )" << endl;
+       Sleep (1000);
+       cout << "                                ." << endl;
+       Sleep (1000);
+       cout << "                                ." << endl;
+       Sleep (1000);
+       cout << "                                ." << endl;
+
+      cout << endl;
+             Sleep (1000);
+
+      cout << endl;
+             Sleep (1000);
+
+       cout << endl;
+              Sleep (1000);
+
+       cout << endl;
+              Sleep (1000);
+       cout << endl;
+        Sleep (1000);
+       cout << endl;
+        Sleep (1000);
+       cout << endl;
+        Sleep (1000);
+       cout << endl;
+        Sleep (1000);
+       cout << endl;
+        Sleep (1000);
+       cout << endl;
+        Sleep (1000);
+       cout << endl;
+        Sleep (1000);
+       cout << endl;
+        Sleep (1000);
+       cout << endl;
+        Sleep (1000);
+       cout << endl;
+        Sleep (1000);     
+       cout << endl;
+        Sleep (1000);
+       cout << endl;
+        Sleep (1000);
+       cout << endl;
+        Sleep (1000);
+       cout << endl;
+        Sleep (1000);
+       cout << endl;
+        Sleep (1000);
+       cout << endl;
+        Sleep (1000);
+       cout << endl;
+        Sleep (1000); 
+      cout << endl;
+        Sleep (1000);
+       cout << endl;
+        Sleep (1000);
+       cout << endl;
+        Sleep (1000);
+   
+       cout << "                      lançamento bem sucedido!"<< endl;
+       cout << endl;
+       cout << endl;
+       cout << endl;
+       cout << endl;
+       cout << endl;
+       cout << endl;
+       cout << endl;
+       cout << endl;
+       cout << endl;
+       cout << endl;
+       cout << endl;
+       cout << endl;
+       cout << endl;
+       cout << endl;
+       cout << endl;      
+  }
